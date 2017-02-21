f7db21ac2b91fb85356b1be7e6cd48e109e45f00
Ritik <saxenar444@gmail.com>
2017-02-22 00:40:53 +0530

    testing with sample ports

--- node.py
+++ node.py
@@ -3,7 +3,7 @@ import socket, select, threading, time, string
 class Node(object):
     busy_nodes = []
     def __init__(self,port,connected_nodes):
-        self.node = socket.socket(family=AF_INET,type=SOCK_DGRAM)
+        self.node = socket.socket(family=socket.AF_INET,type=socket.SOCK_DGRAM)
         self.node.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
         self.port = port
         self.hostname = socket.gethostname()
@@ -19,25 +19,32 @@ class Node(object):
     def run(self):
         count = 0
         busy_neighbour = []
-        for node in connected_nodes:
-            if node not in busy_nodes:
+        for node in self.connected_nodes:
+            print node
+            if node not in Node.busy_nodes:
                 count += 1
             else:
                 busy_neighbour.append(node)
-
-        if count == 2:
+        print "something"
+        if count == len(self.connected_nodes):
+            print "inside count"
+            Node.busy_nodes.append(self.node)
             self.lock_resources()
         else:
+            print "inside else"
             for node in busy_neighbour:
+                print "Node: {0} requesting resources from {1}...".format(self.port,node)
                 threading.Thread(target=self.request_resources,args=(node,)).start()
+                threading.Thread(target=self.receive_requests).start()
 
     def request_resources(self,address):
-        self.node.sendto("node:{0} requesting resources...".format(self.port),(self.hostname,address))
+        print "Node:{0} requesting resources from {1}".format(self.port,address)
+        self.node.sendto("Node: {0} requesting resources...".format(self.port),(self.hostname,address))
 
     def receive_requests(self):
         request, address = self.node.recvfrom(512)
         if string.find(request,"requesting",0,len(request)) > -1:
-            status_response(address)
+            self.status_response(address)
         elif string.find(request,"locked",0,len(request)) > -1:
             time.sleep(1)
             threading.Thread(target=self.request_resources,args=(node,)).start()
@@ -52,20 +59,24 @@ class Node(object):
 
     def status_response(self,address):
         if not self.get_timer():
-            self.node.sendto("resources locked by Node: {0}".format(self.port),(self.hostname,address))
+            print "resources locked by Node: {0}".format(self.port)
+            self.node.sendto("resources locked by Node: {0}".format(self.port),address)
         else:
             self.release_resources(address)
 
     def release_resources(self,address):
         self.locked = False
         Node.busy_nodes.remove(self.node)
-        self.node.sendto("resources released by Node: {0}".format(self.port),(self.hostname,address))
+        print "resources released by Node: {0}".format(self.port)
+        self.node.sendto("resources released by Node: {0}".format(self.port),address)
 
     def start_timer(self):
-        while(self.timer != 15):
+        while(self.timer != 5):
             time.sleep(1)
             self.timer += 1
-        self.reset()
 
     def get_timer(self):
-        return self.timer == 15 ? True : False
+        if self.timer >=15:
+            return True
+        else:
+            return False
---
+++ node1.py
@@ -0,0 +1,7 @@
+from node import Node
+import time
+
+if __name__ == "__main__":
+    time.sleep(5)
+    node = Node(2525,[2230,2235])
+    node.run()
